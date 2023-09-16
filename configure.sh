6cab1702ca9fb8215a2081ff02c332157fe8248b
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-09-15 21:44:44 -0300

    chore: add a configure.sh script

---
+++ configure.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CHECK_FOLDERS='build, bin'
+
+for folder in $(echo "$CHECK_FOLDERS" | sed "s/,/ /g"); do
+  if [ ! -d "$folder" ]; then
+    echo "Creating $folder..."
+    mkdir "$folder"
+  fi
+done
+
+make
